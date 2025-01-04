41e238fb2d1f8fdb2de32a5499de483a14f0308f
mfurniel <mfurnielc@gmail.com>
2025-01-04 18:19:03 -0300

    Delete bad migration
    Create the join table between participants and groups, but participant insertion into groups is not yet available.

--- db/migrate/20241223021231_create_join_table_groups_participants.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGroupsParticipants < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :school_groups, :school_participants do |t|
-      t.index [:school_group_id, :school_participant_id]
-      t.index [:school_participant_id, :school_group_id]
-    end
-  end
-end
