7bacba55bad3a27af262646492e12b9503076482
Rodrigo Campos <ricamphe@gmail.com>
2025-06-25 19:06:27 +0200

    Allows empty email and no birthday for participants

--- app/models/school/participant.rb
+++ app/models/school/participant.rb
@@ -4,10 +4,9 @@ module School
     has_and_belongs_to_many :groups, class_name: "School::Group", join_table: "school_groups_participants", foreign_key: "school_participant_id", association_foreign_key: "school_group_id"
     has_many :session_details, as: :targetable
     validates :name, presence: true, format: { with: /\A[\p{L}\s]+\z/, message: "can only contain letters and spaces" }, length: { minimum: 2, maximum: 60 }
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email" }
-    validates :birthday, presence: true
-    validate :birthday_not_in_the_future
-    validate :birthday_not_too_old
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email" }, allow_blank: true
+    validate :birthday_not_in_the_future, if: -> { birthday.present? }
+    validate :birthday_not_too_old, if: -> { birthday.present? }
 
     def birthday_not_in_the_future
       if birthday.present? && birthday > Date.today
